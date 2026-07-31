a1afc281410624ebbdabd1fdae975e0ffad5ddcf
Happy <subhrajitsamal910@gmail.com>
2026-07-31 23:31:26 +0530

    change in admin dashboard

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,29 +1,14 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection
-} from '@angular/core';
-
-import { provideRouter } from '@angular/router';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
-
 import { routes } from './app.routes';
 
-export const appConfig: ApplicationConfig = {
+  export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(),
-  ],
-};
-    
     provideHttpClient()
   ]
 };
-
