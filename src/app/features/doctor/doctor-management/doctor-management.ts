b1be7ec32025e464ac137414dc6d74773c2065e0
sahoosoumya395-svg <sahoosoumya395@gmail.com>
2026-08-04 19:36:36 +0530

    Implemented appointment booking UI changes

--- src/app/features/doctor/doctor-management/doctor-management.ts
+++ src/app/features/doctor/doctor-management/doctor-management.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-doctor-management',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './doctor-management.html',
   styleUrl: './doctor-management.css'
 })
